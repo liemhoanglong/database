ad8107631b8eb16b7917421a0760a3a91c12319d
Your Name <liemhoanglong@gmail.com>
2019-11-12 12:56:09 +0700

    edit

--- routes/index.js
+++ routes/index.js
@@ -8,60 +8,60 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/demo-express');
 
 //3.tạo Schema
-var userDataSchema = new  mongoose.Schema({
-  name: {type: String, required: true},
-  age: Number,
-  pass: String
-}, {collection: 'user-data'});
+var userDataSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	age: Number,
+	pass: String
+}, { collection: 'user-data' });
 
 //4.tạo model
 var UserData = mongoose.model('UserData', userDataSchema);
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 });
 
-router.get('/get-data', function(req, res, next) {
-  UserData.find()
-      .then(function(doc) {
-        res.render('index', {items: doc});
-      });
+router.get('/get-data', function (req, res, next) {
+	UserData.find()
+		.then(function (doc) {
+			res.render('index', { items: doc });
+		});
 });
 
-router.post('/insert', function(req, res, next) {
-  var item = {
-    name: req.body.name,
-    age: req.body.age,
-    pass: req.body.pass
-  };
+router.post('/insert', function (req, res, next) {
+	var item = {
+		name: req.body.name,
+		age: req.body.age,
+		pass: req.body.pass
+	};
 
-  var data = new UserData(item);
-  data.save();
+	var data = new UserData(item);
+	data.save();
 
-  res.redirect('/');
+	res.redirect('/');
 });
 
-router.post('/update', function(req, res, next) {
-  var id = req.body.id;
+router.post('/update', function (req, res, next) {
+	var id = req.body.id;
 
-  UserData.findById(id, function(err, doc) {
-    if (err) {
-      console.error('error, no entry found');
-    }
-    doc.name = req.body.name;
-    doc.age = req.body.age;
-    doc.pass = req.body.pass;
-    doc.save();
-  })
-  res.redirect('/');
+	UserData.findById(id, function (err, doc) {
+		if (err) {
+			console.error('error, no entry found');
+		}
+		doc.name = req.body.name;
+		doc.age = req.body.age;
+		doc.pass = req.body.pass;
+		doc.save();
+	})
+	res.redirect('/');
 });
 
-router.post('/delete', function(req, res, next) {
-  var id = req.body.id;
-  UserData.findByIdAndRemove(id).exec();
-  res.redirect('/');
+router.post('/delete', function (req, res, next) {
+	var id = req.body.id;
+	UserData.findByIdAndRemove(id).exec();
+	res.redirect('/');
 });
 
 module.exports = router;
